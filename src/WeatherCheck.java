6d2fdcdafc895611ef39b1a9340351b3b3bafe92
Lamees R <131818469+Lameeshennaarts39@users.noreply.github.com>
2023-07-06 20:55:18 +0100

    extension tast working on it

---
+++ src/WeatherCheck.java
@@ -0,0 +1,12 @@
+public class WeatherCheck {
+
+    public static void main(String[] args) {
+        int currentTemperature = 25;
+        String currentRaining = "Yes";
+
+        if (currentRaining.equals("Yes")) {
+            System.out.println("wear a waterproof coat.");
+        }
+
+    }
+}
