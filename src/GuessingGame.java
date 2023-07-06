08e34e7b52da9246bd58feedfaa7a20e641eaa33
Lamees R <131818469+Lameeshennaarts39@users.noreply.github.com>
2023-07-06 19:19:41 +0100

    correct text

---
+++ src/GuessingGame.java
@@ -0,0 +1,13 @@
+public class GuessingGame {
+
+    public static void main(String[] args) {
+        int secretNumber = 5;
+        int usersGuess = 5;
+
+        if (usersGuess == secretNumber) {
+            System.out.println("Congratulations, you win!");
+
+        }
+
+    }
+}
