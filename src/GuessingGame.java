12ec7a25737ba455863905baf7fb811ed7584b79
Lamees R <131818469+Lameeshennaarts39@users.noreply.github.com>
2023-07-06 20:09:02 +0100

    changed spellings

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -2,12 +2,19 @@ public class GuessingGame {
 
     public static void main(String[] args) {
         int secretNumber = 7;
-        int usersGuess = 7;
+        int usersGuess = 3;
 
         if (usersGuess == secretNumber) {
-            System.out.println("you won!");
+            System.out.println("you are correct!");
 
+        } else {
+            if (usersGuess > secretNumber) {
+        System.out.println("Nope, your guess is too high!");
+        } else {
+          if (usersGuess < secretNumber) {
+                System.out.println("Yes, your guess is too low!");
+    }
+}
         }
-
     }
 }
